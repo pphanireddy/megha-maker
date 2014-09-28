a121e37626451a5302720c83affce6ce351d3c09
Phani <pphanireddy@gmail.com>
2014-09-28 16:45:31 -0700

    Updating the fab file

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 
 import fabric.api
+def add():
+    fabric.api.local('git add -p')
 
 def commit():
-    fabric.api.local('git add -p && git commit')
+    fabric.api.local('git commit')
 
 def push():
     fabric.api.local('git push')
 
 def prepare_deploy():
+    add()
     commit()
     push()
