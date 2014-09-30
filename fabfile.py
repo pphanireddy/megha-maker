d9cb939105525ea3f0466f94165bfb15e9bf3517
Phani <pphanireddy@gmail.com>
2014-09-29 21:40:16 -0700

    dd comments

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 
 import fabric.api
+
+# Add all files except ignored files to git
 def add():
     fabric.api.local('git add -A')
 
+# Commit added files to git
 def commit():
     fabric.api.local('git commit')
 
+# Push the committed changes to github
 def push():
     fabric.api.local('git push')
 
+# Execute the three steps needed to update the latest code
 def prepare_deploy():
     add()
     commit()
