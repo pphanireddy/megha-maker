6db79288defe1657e59daf614c34caebc28f1da0
blr <pphanireddy@gmail.com>
2014-10-06 21:10:01 -0700

    update deploy tasks

--- deploy.py
+++ deploy.py
@@ -7,9 +7,15 @@ from config import configdict
 # installs the standard packages required by the service
 @fabric.api.task
 def install_packages():
-    fabric.api.local('git add -A')
+    fabric.api.run(r'sudo apt-get install -y python-software-properties')
+    fabric.api.run(r'sudo add-apt-repository -y ppa:webupd8team/java')
+    fabric.api.run(r'sudo apt-get update')
+    fabric.api.run(r'sudo apt-get install -y oracle-java7-installer')
 
 # installs hadoop
 @fabric.api.task
 def install_hadoop():
-    fabric.api.local('git commit')
+    fabric.api.run(r'rm -rf ~/hadoop')
+    fabric.api.run(r'wget http://apache.mirrors.spacedump.net/hadoop/common/stable/hadoop-2.5.1.tar.gz')
+    fabric.api.run(r'tar xvf hadoop-2.5.1.tar.gz --gzip ~/hadoop')
+    fabric.api.run(r'rm hadoop-2.5.1.tar.gz')
